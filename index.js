aa108d421f3757ee86d7cd2ea9ba8b899321bd02
patrickgilsf <johnpatrickgilligan@gmail.com>
2022-02-17 17:03:58 -0800

    vid mute was working, no longer is...questioning sanity now

--- index.js
+++ index.js
@@ -1,43 +1,78 @@
+//node HID import
 var HID = require('node-hid');
 
-
-
-
+//Device in for for ymd09
 var VENDOR_ID = 0x594D; //22861
 var PRODUCT_ID = 0x4409; //17417
 var USAGE_PAGE = 0xFF60; //65376
 var USAGE = 0x61; //97
-var path = 'IOService:/AppleARMPE/arm-io@10F00000/AppleT810xIO/usb-drd1@2280000/AppleT8103USBXHCI@01000000/usb-drd1-port-hs@01100000/YMD09@01100000/IOUSBHostInterface@1/AppleUserUSBHostHIDDevice';
+//var path = 'IOService:/AppleACPIPlatformExpert/PCI0@0/AppleACPIPCI/XHC1@14/XHC1@14000000/HS03@14300000/USB2.0 Hub@14300000/AppleUSB20Hub@14300000/AppleUSB20HubPort@14310000/SW4 USB PLUS-2P HUB@14310000/AppleUSB20Hub@14310000/AppleUSB20HubPort@14312000/SW4 USB PLUS-4P HUB@14312000/AppleUSB20Hub@14312000/AppleUSB20HubPort@14312200/USB2.0 Hub@14312200/AppleUSB20Hub@14312200/AppleUSB20HubPort@14312240/YMD09@14312240/IOUSBHostInterface@1/AppleUserUSBHostHIDDevice';
 var deviceList = HID.devices();
-var device = new HID.HID(path);
-    
-
-//logs HID devices
-
-
-
-
-// device.sendFeatureReport([0x01, 'c', 0, 0xff,0x33,0x00, 70,0, 0]);
-
-
-
-muteMic = () => {
-    device.write([0x00, 0x01, 0x01, 0x01, 0x01, 0x01])
-}
-
-unmuteMic = () => {
-    device.write([0x00, 0x02, 0x02, 0x02, 0x02, 0x02])
-}
-
-//muteMic();
-unmuteMic();
-
-
-
-//console logs
 //console.log(deviceList);
-//console.log('devices:', HID.devices());
-//console.log(device.getFeatureReport());
-//console.log(device)
-//console.log(VENDOR_ID,PRODUCT_ID,USAGE_PAGE,USAGE);
-//console.log(process.argv);
+var path = ''; 
+deviceList.forEach(item => {
+  if (item.usagePage == USAGE_PAGE) {
+    path = item.path;
+  }
+});
+var device = new HID.HID(path);
+
+const muteMic = () => {
+    device.write([0x00, 0x01, 0x01, 0x01, 0x01, 0x01])
+};
+const unmuteMic = () => {
+    device.write([0x00, 0x02, 0x02, 0x02, 0x02, 0x02])
+};
+const muteVid = () => {
+  device.write([0x00, 0x03, 0x03, 0x03, 0x03, 0x03])
+};
+const unmuteVid = () => {
+  device.write([0x00, 0x04, 0x04, 0x04, 0x04, 0x04])
+};
+
+
+  muteMic()
+  // unmuteMic()
+  muteVid()
+  // unmuteVid()
+
+
+
+
+
+//Feedback not working
+// device.on('error', (err) => {
+//   console.log(err);
+//   device.close();
+// });
+
+// device.on('data', (data) => {
+//   if (data) {
+//     console.log(data)
+//   }
+// })
+
+//need feedback for this to work
+// var micMute = false;
+// var vidMute = false;
+// const toggleMicMute = () => {
+//   if (micMute) {
+//     unmuteMic();
+//   } else {
+//     muteMic();
+//   }
+// };
+
+// const toggleVidMute = () => {
+//   if (vidMute) {
+//     unmuteVid();
+//   } else {
+//     muteVid();
+//   }
+// }
+
+// toggleMicMute();
+// toggleVidMute();
+
+
+
